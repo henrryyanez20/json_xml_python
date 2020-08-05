1b0fec358efac6a721b9096011475d24cfd0fb77
Henrry <64995880+henrryyanez20@users.noreply.github.com>
2020-08-05 00:53:22 -0300

    Transformacion de numero listo falta agrupar
    Falta agrupar el resultado por UserId, por lo que debo hacer otra lista por comprensión para completarlo y luego poder hacer el report.

--- ejercicio_hackerrank.py
+++ ejercicio_hackerrank.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import re
 import xml.etree.ElementTree as ET
 from datetime import datetime
 
@@ -13,19 +14,25 @@ def fetch(page_number, location_id):
     # def fetch():
     url = 'https://jsonmock.hackerrank.com/api/transactions/search?txnType=debit&page=' + page_number
     respuesta = requests.get(url)
-    dataset = respuesta.json()
-    a = dataset.get("data")
-    filtro = [{'User': x['userId'], 'Monto': x['amount']} for x in a if x.get('location').get('id') == location_id]
-    print(filtro)
+    respuesta2 = respuesta.json()
+    a = respuesta2.get("data")
+    datas = [{'Monto': x['amount'], 'User': x['userId']} for x in a if x.get('location').get('id') == location_id]
+    #dataset = json.dumps(datas)
+    return datas
 
     #c = [i for i in a if i.get('location').get('id') == location_id]
     #print(c)
     #print(c[0]['userId'], c[0]['amount'])
 
-    # b = json.dumps(c, indent=4)
-    # print(b)
+def transform(datas):
+    #a2 = datas[2]['Monto']
+    a2 = [{'Monto2': float(re.sub(r'[^\d\-.]', '', x['Monto'])), 'User2': x['User']} for x in datas]
+    print(a2)
 
+    
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    fetch(str(7), 8)
+    datas = fetch(str(12), 8)
+    df = transform(datas)
+    #report(df)
